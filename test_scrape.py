c23feeef84819c3051dcac8345656064f9fc3546
AJStangl <ajstangl@gmail.com>
2022-10-15 07:53:35 -0400

    Fuck reddit

--- test_scrape.py
+++ test_scrape.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 from shared_code.clients.push_shift_client import PushShiftClient
@@ -22,8 +23,52 @@ load_dotenv()
 logger = logging.getLogger("training")
 
 
-def main():
+class DataWrapper(object):
+	def __init__(self):
+		self.comment_collection_thread = threading.Thread(target=self.comment_collection_thread, args=(), daemon=True)
+		self.submission_collection_thread = threading.Thread(target=self.submission_collection_thread, args=(),
+															 daemon=True)
+		self.client: PushShiftClient = PushShiftClient()
 
+	def comment_collection_thread(self):
+		before = None
+		i = 0
+		while True:
+			comments: dict = self.client.get_author_comments("ReallyRickAstley", before=before)
+			if not comments:
+				break
+
+			all_comment_ids: [str] = [comment.get('id') for comment in comments]
+			found_comment_ids = context.search_by_id(all_comment_ids, db_session)
+			remaining_comments_ids = [item for item in all_comment_ids if item not in found_comment_ids]
+			logger.info(f"{len(remaining_comments_ids)} from current batch {len(comments)} remains...")
+			for comment in comments:
+				before = comment['created_utc']
+				if comment.get('id') not in remaining_comments_ids:
+					continue
+				else:
+					data_row: TrainingDataRow = self.client.handle_comment(comment)
+					result = context.add(data_row, db_session)
+					if result is None:
+						logger.info(f"Added {i} rows")
+						i += 1
+					else:
+						logger.info(f"Nothing was updated for Comment {data_row.CommentId}")
+						continue
+					time.sleep(1)
+			logger.info("Continuing...")
+		logger.info("Complete")
+		return before
+
+	def submission_collection_thread(self):
+		pass
+
+	def run(self):
+		self.comment_collection_thread.start()
+		self.submission_collection_thread.start()
+
+
+def main():
 	client: PushShiftClient = PushShiftClient()
 	before = None
 	i = 0
